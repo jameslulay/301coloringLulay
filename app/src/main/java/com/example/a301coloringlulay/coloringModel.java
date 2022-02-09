8248d2b0e0d6ff84bfa78e4d3d7f4ced41b59f8c
jameslulay <lulay21@up.edu>
2022-02-08 20:59:27 -0800

    safdsafdfdsfsda

--- app/src/main/java/com/example/a301coloringlulay/coloringModel.java
+++ app/src/main/java/com/example/a301coloringlulay/coloringModel.java
@@ -10,22 +10,24 @@ public class coloringModel {
 
     public int currentRGB = Color.rgb(tempRedValue,tempGreenValue,tempBlueValue);
 
-    public int circ1RGB = Color.rgb(200,200,200),
-               circ2RGB = Color.rgb(100,100,100),
-               circ3RGB = Color.rgb(100,100,100),
-               circ4RGB = Color.rgb(100,100,100),
-               circ5RGB = Color.rgb(100,100,100),circ6RGB;
-
-
-
-
-
-
-
-
-
-
-
+    public int circ1Red = 100,
+               circ1Green = 100,
+               circ1Blue = 100,
+               circ2Red = 100,
+               circ2Green = 100,
+               circ2Blue = 100,
+               circ3Red = 100,
+               circ3Green = 100,
+               circ3Blue = 100,
+               circ4Red = 100,
+               circ4Green = 100,
+               circ4Blue = 100,
+               circ5Red = 100,
+               circ5Green = 100,
+               circ5Blue = 100,
+               circ6Red = 100,
+               circ6Green = 100,
+               circ6Blue = 100;
 
 
 
